af2468ef894ceb4e14499f96d8c5d774bf9c723a
Kelly Yong <kelly.a.yong@gmail.com>
2021-02-18 01:18:25 -0500

    Add event listener to each grid and turn background black when mouseover

--- index.js
+++ index.js
@@ -12,3 +12,14 @@ let totalGrid = 256;
 for (let i = 0; i<totalGrid; i++) {
     createGrid();
 };
+
+// events
+const grids = document.getElementsByClassName('grid');
+for(let i=0; i < grids.length; i++) {
+    Array.from(grids);
+    grids[i].addEventListener('mouseover', changeColor);
+}
+
+function changeColor(e) {
+    e.target.style.backgroundColor = 'black';
+};
